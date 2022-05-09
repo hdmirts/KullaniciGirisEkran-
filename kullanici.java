afb853c0568d1e175d6a326e50c6854aa553c848
hatice demirtas <98237665+hdmirts@users.noreply.github.com>
2022-05-10 00:28:40 +0300

    Create kullanici.java

---
+++ kullanici.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class kullanici {
+    public static void main(String[] args) {
+        String kullaniciAd, sifre;
+        
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Kullanıcı Adınız :");
+        kullaniciAd = inp.nextLine();
+        System.out.print("Şifreniz :");
+        sifre = inp.nextLine();
+    
+
+        if (kullaniciAd.equals("patika") && sifre.equals("java123")) {
+            System.out.println("Giriş Yaptınız");
+
+        } else {
+            System.out.println("Bilgileriniz Yanlış");
+           
+
+        }
+
+
+    }
+
+}
+    
